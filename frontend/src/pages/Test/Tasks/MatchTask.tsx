ca90c89f505469285f40a4b4f55b91f86e6ce997
Gilián Erik <gilianerik1107@gmail.com>
2026-01-21 23:24:35 +0100

    feat: enhance MatchTask component with answer management and dynamic options

--- frontend/src/pages/Test/Tasks/MatchTask.tsx
+++ frontend/src/pages/Test/Tasks/MatchTask.tsx
@@ -1,11 +1,63 @@
+import { useCallback, useEffect } from "react";
 import { usePracticeStore } from "../../../stores/practice.store";
+import type { MatchOptions } from "../../../types/task.type";
+import Select from "react-select";
+import { selectStyles } from "../../../modules/select.module";
 
 const MatchTask = () => {
-  const { currentTask } = usePracticeStore();
+  const { currentTask, answers, setAnswer } = usePracticeStore();
+
+  useEffect(() => {
+    const answer = answers.get(currentTask?.id || "") as MatchOptions;
+    if (answer) return;
+
+    const options = currentTask?.options as unknown as MatchOptions;
+    const initial = {
+      groups: options.groups,
+      items: options.items.map((item) => ({
+        text: item.text,
+        group: "",
+      })),
+    };
+
+    setAnswer(currentTask?.id || "", initial);
+  }, []);
+
+  const getOptions = useCallback(() => {
+    const options = answers.get(currentTask?.id || "") as MatchOptions;
+    return options;
+  }, [answers]);
 
   return (
     <div>
-      <p>{currentTask?.description}</p>
+      <p style={{ marginBottom: "20px" }}>{currentTask?.description}</p>
+      {getOptions()?.items.map((item, index) => (
+        <div
+          key={index}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "10px",
+            marginBottom: "10px",
+          }}
+        >
+          <Select
+            styles={selectStyles}
+            options={getOptions()?.groups.map((g) => ({ value: g, label: g }))}
+            onChange={(e) => {
+              console.log(e);
+              const group = e ? (e as { value: string }).value : "";
+
+              const options = getOptions();
+              options.items[index].group = group;
+              
+              setAnswer(currentTask?.id || "", options);
+            }}
+            value={{value: item.group, label: item.group}}
+          />
+          <p>{item.text}</p>
+        </div>
+      ))}
     </div>
   );
 };
