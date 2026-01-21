1e162a3e82f4ae81ec11f7b0bf692258542e9889
Gilián Erik <gilianerik1107@gmail.com>
2026-01-21 16:06:30 +0100

    feat: add check constraint for questionCount in TestTable

--- api/src/database/models/test.model.ts
+++ api/src/database/models/test.model.ts
@@ -1,7 +1,8 @@
-import { pgTable, varchar, uuid, integer } from "drizzle-orm/pg-core";
+import { pgTable, varchar, uuid, integer, check } from "drizzle-orm/pg-core";
 import { withTimestamps } from "../cols/timestamp.col";
 import { withCreatedBy } from "../cols/created-by.col";
 import { CategoryTable } from "./category.model";
+import { sql } from "drizzle-orm";
 
 export type Test = typeof TestTable.$inferSelect;
 
@@ -13,14 +14,23 @@ export type UpdateTestArgs = Partial<
   Omit<CreateTestArgs, "id" | "createdBy" | "categoryId">
 >;
 
-export const TestTable = pgTable("tests", {
-  id: uuid("id").defaultRandom().primaryKey(),
-  name: varchar({ length: 255 }).notNull(),
-  questionCount: integer("question_count").notNull().default(0),
-  categoryId: uuid("category_id")
-    .notNull()
-    .references(() => CategoryTable.id, { onDelete: "cascade" }),
+export const TestTable = pgTable(
+  "tests",
+  {
+    id: uuid("id").defaultRandom().primaryKey(),
+    name: varchar({ length: 255 }).notNull(),
+    questionCount: integer("question_count").notNull().default(0),
+    categoryId: uuid("category_id")
+      .notNull()
+      .references(() => CategoryTable.id, { onDelete: "cascade" }),
 
-  ...withCreatedBy(),
-  ...withTimestamps(),
-});
+    ...withCreatedBy(),
+    ...withTimestamps(),
+  },
+  (table) => ({
+    questionCountCheck: check(
+      "question_count_check",
+      sql`${table.questionCount} >= 0`,
+    ),
+  }),
+);
